55ea8383b542db094ee3c8073cb57975a4499f3d
mineduck1608 <ducdm160804@gmail.com>
2024-08-16 17:52:22 +0700

    1

---
+++ WebAPIApp/WebAPIApp/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIApp.Models;
+
+namespace WebAPIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<Product> products = new List<Product>();
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM)
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = productVM.Name,
+                Price = productVM.Price,
+            };
+            products.Add(product);
+            return Ok(products);
+        }
+    }
+}
---
+++ WebAPIApp/WebAPIApp/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPIApp.Models
+{
+    public class ProductVM
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Product : ProductVM
+    {
+        public Guid Id { get; set; }
+    }
+}
